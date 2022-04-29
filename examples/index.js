363834b930c9800263a2f6553069e0281fae5bc3
nuintun <nuintun@qq.com>
2022-04-29 14:30:12 +0800

    ✨ Update examples

--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,7 @@
 const LinkedList = require('./LinkedList');
 
-const x = new LinkedList([1, 2, 3, 4, 5]);
+const linked = new LinkedList([1, 2, 3, 4, 5]);
 
-console.log('Removed:', x.splice(2, 1, 68, 86));
-console.log('Sliced :', x.slice().valueOf());
-console.log('Linked :', x.valueOf());
+console.log('Removed:', linked.splice(2, 1, 68, 86));
+console.log('Sliced :', linked.slice().valueOf());
+console.log('Linked :', linked.valueOf());
